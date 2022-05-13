27c88e1894a96ad0255fc8ac66b095516d147949
Luciobio <pardobio@gmail.com>
2022-05-13 11:17:33 -0300

    first commit

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import logo from '../../icons/RCNY.png'
+import homebkg from '../../images/posters/HERO3_shade.jpg'
+import './Home.css'
+
+
+const Home = () => {
+    return (
+        <div className='home'>
+            <img 
+                className='home-bkg'
+                src={homebkg}
+                alt=''
+            />
+            <div className='intro-poster'>
+                <img src={logo} className='logo' alt="Logo"/>
+                <div className='start-btn btn'>
+                <a className='web-link2 white-link'href='http://186.138.251.141'>
+                    Inmersive Experience
+                </a>
+                </div>
+            </div>
+        </div>
+    )
+
+}
+
+export default Home
