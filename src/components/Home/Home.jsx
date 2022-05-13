ce482958e88dc5591a3367754c33c24aee447f3a
Luciobio <pardobio@gmail.com>
2022-05-13 11:19:49 -0300

    console warning fixed

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
 import logo from '../../icons/RCNY.png'
 import homebkg from '../../images/posters/HERO3_shade.jpg'
 import './Home.css'
